5a954731eeec3e08aa0ecd9e30913056c65edd88
Pereiralaisa <129809535+Pereiralaisa@users.noreply.github.com>
2023-05-08 17:53:44 -0300

    Update SOUSA.py

--- SOUSA.py
+++ SOUSA.py
@@ -34,8 +34,10 @@ column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
 df = df[df['state'] == state]
 
 
-#fig = px.line(df, x="data", y=column, title=column + ' - ' + state)
-#fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
+fig = px.line(df, x="data", y=column, title=column + ' - ' + state)
+fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
+colors = px.colors.qualitative.swatches()
+colors.show()
 
 st.title('DADOS COVID-19')
 st.write('Nessa aplicação, o usuário tem a possibilidade de interação,e visualização de dados sobre  covid-19. Utilize o menu lateral para alterar a mostragem.')
