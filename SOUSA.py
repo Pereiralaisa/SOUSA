0d5b75efd4c8d60005ffa6a102f49e851a97fca3
Pereiralaisa <129809535+Pereiralaisa@users.noreply.github.com>
2023-05-08 20:54:37 -0300

    Update SOUSA.py

--- SOUSA.py
+++ SOUSA.py
@@ -34,9 +34,39 @@ column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
 
 df = df[df['state'] == state]
 
-data_canada = px.data.gapminder().query("country == 'Canada'")
-fig = px.bar(data_canada, x='year', y='pop')
-fig.show()
+def validate():
+    if origin.value in df['origin'].unique() and textbox.value in df['carrier'].unique():
+        return True
+    else:
+        return False
+
+
+def response(change):
+    if validate():
+        if use_date.value:
+            filter_list = [i and j and k for i, j, k in
+                           zip(df['month'] == month.value, df['carrier'] == textbox.value,
+                               df['origin'] == origin.value)]
+            temp_df = df[filter_list]
+
+        else:
+            filter_list = [i and j for i, j in
+                           zip(df['carrier'] == 'DL', df['origin'] == origin.value)]
+            temp_df = df[filter_list]
+        x1 = temp_df['arr_delay']
+        x2 = temp_df['dep_delay']
+        with g.batch_update():
+            g.data[0].x = x1
+            g.data[1].x = x2
+            g.layout.barmode = 'overlay'
+            g.layout.xaxis.title = 'Delay in Minutes'
+            g.layout.yaxis.title = 'Number of Delays'
+
+
+origin.observe(response, names="value")
+textbox.observe(response, names="value")
+month.observe(response, names="value")
+use_date.observe(response, names="value")
 
 
 #fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
