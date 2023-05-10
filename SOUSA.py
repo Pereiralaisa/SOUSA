e2a64bd4a0392547880faee35b541a63fadc4b5b
Pereiralaisa <129809535+Pereiralaisa@users.noreply.github.com>
2023-05-09 22:53:56 -0300

    Update SOUSA.py

--- SOUSA.py
+++ SOUSA.py
@@ -34,6 +34,11 @@ column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
 
 df = df[df['state'] == state]
 
+dados_x=['2020','2021']
+dados_y=[10,20,5,35]
+
+fig=px.pie(names=dados_x, values=dados_y, width=300, height=300)
+fig.show()
 
 #fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
 fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
